ca80539e5973e20eac7a4fd16365f615cbc9b8a0
Azat <minyazovazat@gmail.com>
2026-01-30 17:28:13 +0300

    secnod task

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +17,8 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def receipts(request, name):
+    recipe = DATA.get(name, {})
+    return render(request, "calculator/index.html", { "recipe": recipe })
